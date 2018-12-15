adb194337e110f19f9e14742b67f663d8eebdcde
karkin96 <karkin.ai@mail.ru>
2018-12-16 02:19:46 +0300

    remove old Lab7 version

--- Lab7/app/src/androidTest/java/iu9/bmstu/ru/lab7/ExampleInstrumentedTest.java
+++ Lab7/app/src/androidTest/java/iu9/bmstu/ru/lab7/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package labs.android.iu9.bmstu.ru.lab7;
+package iu9.bmstu.ru.lab7;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("labs.android.iu9.bmstu.ru.lab7", appContext.getPackageName());
+        assertEquals("iu9.bmstu.ru.lab7", appContext.getPackageName());
     }
 }
